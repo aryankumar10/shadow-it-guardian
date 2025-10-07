8cb62becd313e6b0a13a6a62d04b3ab51dc954ad
Aryan Kumar <aryann.kumar2000@gmail.com>
2025-10-07 14:27:25 -0700

    changes to logic

--- app.py
+++ app.py
@@ -10,23 +10,48 @@ def home():
     return "Hello, World! Your Flask app is running. 🚀"
 
 # Define a route that returns JSON data
-@app.route('/api/data')
+@app.route('/api/employees_current')
 def get_data():
-    """This route returns a sample JSON object."""
-    # This data could come from a database, another API, etc.
     sample_data = {
-    "approved": {
-        "onedrive": "Secure file storage and sharing",
-        "miro": "Collaborative design tool"
-    },
-    "unapproved": {
-        "dropbox": "Not approved for enterprise use",
-        "figma": "Not approved for enterprise use",
-        "chatgpt": "Not approved for enterprise use"
+        "IT Manager": 2,
+        "IT Developer": 6,
+        "LOGISTIC Manager": 4,
+        "LOGISTIC Employee": 8
     }
-}
-    # jsonify converts the Python dictionary to a JSON response
     return jsonify(sample_data)
+
+
+@app.route('/api/employees_min')
+def get_data():
+    sample_data = {
+        "IT Manager": 1,
+        "IT Developer": 4,
+        "LOGISTIC Manager": 2,
+        "LOGISTIC Employee": 8
+    }
+    return jsonify(sample_data)
+
+@app.route('/api/employees_depts')
+def get_data():
+    sample_data = {
+        "everett.allen741@moveworkslabs.com": "IT Manager",
+        "it_manager@moveworkslabs.com": "IT Manager",
+        "it_developer@moveworkslabs.com": "IT Developer",
+        "logistic_manager@moveworkslabs.com": "LOGISTIC Manager",
+        "logistic_employee@moveworkslabs.com": "LOGISTIC Employee"
+    }
+    return jsonify(sample_data)
+
+@app.route('/api/depts_msgs')
+def get_data():
+    sample_data = {
+        "IT Manager": "Cannot take leave due to minimum IT Manager required rule",
+        "IT Developer": "Cannot take leave due to minimum IT Developer required rule",
+        "LOGISTIC Manager": "Cannot take leave due to minimum LOGISTIC Manager required rule",
+        "LOGISTIC Employee": "Cannot take leave due to minimum LOGISTIC Employee required rule"
+    }
+    return jsonify(sample_data)
+
 
 # This is only for local testing, Vercel will use its own server
 if __name__ == '__main__':
