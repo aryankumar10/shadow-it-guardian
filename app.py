ac64d734cb2c5334ddb871d328e3e85ef4de3e0b
Aryan Kumar <aryann.kumar2000@gmail.com>
2025-10-07 15:43:40 -0700

    bugfix

--- app.py
+++ app.py
@@ -18,13 +18,20 @@ def get_data_1():
         "LOGISTIC Manager": 4,
         "LOGISTIC Employee": 8
     }
-    user_dept = request.args.get('userdept').lower()
-    if user_dept:
-        department = sample_data.get(user_dept).lower()
-        if department:
-            return jsonify({"department": department})
+    user_dept_query = request.args.get('userdept')
+
+    if user_dept_query:
+        found_key = None
+        for key in sample_data.keys():
+            if key.lower() == user_dept_query.lower():
+                found_key = key
+                break
+        
+        if found_key:
+            counts = sample_data.get(found_key)
+            return jsonify({"counts": counts})
         else:
-            return jsonify({"error": "User email not found"}), 404
+            return jsonify({"error": "Department not found"}), 404
     else:
         return jsonify(sample_data)
 
@@ -37,13 +44,20 @@ def get_data_2():
         "LOGISTIC Manager": 2,
         "LOGISTIC Employee": 8
     }
-    user_dept = request.args.get('userdept').lower()
-    if user_dept:
-        department = sample_data.get(user_dept).lower()
-        if department:
-            return jsonify({"department": department})
+    user_dept_query = request.args.get('userdept')
+
+    if user_dept_query:
+        found_key = None
+        for key in sample_data.keys():
+            if key.lower() == user_dept_query.lower():
+                found_key = key
+                break
+        
+        if found_key:
+            counts = sample_data.get(found_key)
+            return jsonify({"counts": counts})
         else:
-            return jsonify({"error": "User email not found"}), 404
+            return jsonify({"error": "Department not found"}), 404
     else:
         return jsonify(sample_data)
 
@@ -77,13 +91,20 @@ def get_data_4():
         "LOGISTIC Manager": "Cannot take leave due to minimum LOGISTIC Manager required rule",
         "LOGISTIC Employee": "Cannot take leave due to minimum LOGISTIC Employee required rule"
     }
-    user_dept = request.args.get('userdept').lower()
-    if user_dept:
-        department = sample_data.get(user_dept).lower()
-        if department:
-            return jsonify({"department": department})
+    user_dept_query = request.args.get('userdept')
+
+    if user_dept_query:
+        found_key = None
+        for key in sample_data.keys():
+            if key.lower() == user_dept_query.lower():
+                found_key = key
+                break
+        
+        if found_key:
+            message = sample_data.get(found_key)
+            return jsonify({"message": message})
         else:
-            return jsonify({"error": "User email not found"}), 404
+            return jsonify({"error": "Department not found"}), 404
     else:
         return jsonify(sample_data)
 
